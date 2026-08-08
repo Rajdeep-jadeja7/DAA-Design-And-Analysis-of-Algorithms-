c5300c78fd2ff0274dd63cf097d45276b2bf5e22
JADEJA RAJDEEPSINH <rajdeepjadeja2006@gmail.com>
2026-08-08 19:01:11 +0530

    Time: 14 ms (13.22%), Space: 19.4 MB (54.92%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -10,7 +10,7 @@ class Solution:
         return intersection  
         """
 
-        if(len(nums1)<=len(nums2)):
+        if(len(nums1)<len(nums2)):
             for i in nums1:
                 if i in nums2:
                     intersection.append(i)
