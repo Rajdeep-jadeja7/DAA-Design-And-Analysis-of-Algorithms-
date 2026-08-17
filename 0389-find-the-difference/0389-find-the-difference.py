b36c4555d85f788c3373db4fcadff9a2a66f8f5a
JADEJA RAJDEEPSINH <rajdeepjadeja2006@gmail.com>
2026-08-17 13:17:20 +0530

    Time: 4 ms (23.52%), Space: 19.2 MB (62.29%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,11 +1,11 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        n=""
+        
         for i in t:
             if i in s:
                 s=s.replace(i,"",1)
             else:    
-                 n+=i
-        return n       
+                 return i
+          
 
         
