f6cb602bff60a3dcc37b74ee4c96184918810948
JADEJA RAJDEEPSINH <rajdeepjadeja2006@gmail.com>
2026-09-02 10:17:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (47.47%) - LeetHub-Neo

---
+++ DAA-Leetcode/3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+        #the output will always be true 
+        #because if all the numbers are even then true 
+        #and if all numbers are not even then we can make the entire array of odd numbers
+        #bcoz even - odd = odd
+        # even + odd = odd
+        # so output will be always true
+
+
+       
+
+
+
+        
