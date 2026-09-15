aa3e68a6ef579111aa2d821b1bd57b52aa6a4dce
JADEJA RAJDEEPSINH <rajdeepjadeja2006@gmail.com>
2026-09-15 09:20:34 +0530

    Time: 0 ms (100%), Space: 19.4 MB (20.34%) - LeetHub-Neo

---
+++ DAA-Leetcode/2942-find-words-containing-character/2942-find-words-containing-character.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        indices=[]
+        for i in range(len(words)):
+            if x in words[i]:
+                indices.append(i)
+
+        return indices    
+            
+        
