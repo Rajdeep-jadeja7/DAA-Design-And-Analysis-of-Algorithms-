bcad3e2d68b98f938499901343f7292673eca5d3
JADEJA RAJDEEPSINH <rajdeepjadeja2006@gmail.com>
2026-09-17 19:42:52 +0530

    Time: 33 ms (49.35%), Space: 33.8 MB (34.89%) - LeetHub-Neo

---
+++ DAA-Leetcode/2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        less=[]
+        equal=[]
+        greater=[]
+
+        for i in nums:
+            if i<pivot:
+                less.append(i)
+            elif i==pivot:
+                equal.append(i)
+            elif i>pivot:
+                greater.append(i)  
+
+        return less+equal+greater              
+
+
+
+        
